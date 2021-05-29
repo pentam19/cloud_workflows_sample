e9b0510724e8ac60798fd7ccefe402b52c65dbe9
masahirot <13981959+pentam19@users.noreply.github.com>
2021-05-29 17:26:48 +0900

    Cloud Workflows Sample

---
+++ README.md
@@ -0,0 +1,8 @@
+# cloud_workflows_sample
+#### 参考
+* https://cloud.google.com/workflows/docs/reference/syntax#arrays
+* https://cloud.google.com/workflows/docs/sample-workflows?hl=ja#post-requestion-function
+#### Result
+{
+  "concat_result": "foo:ba:r:"
+}
---
+++ cloud_functions/main.py
@@ -0,0 +1,21 @@
+def hello_world(request):
+    print(request)
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    print(request_json)
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    elif 'message_from_workflow' in request_json:
+        print(request_json['message_from_workflow'])
+        return ':'
+    else:
+        return f'Hello World!'
